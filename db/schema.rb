433d182e40cacc44436948b41a5663586029a1b4
CREMA <crema@FAC1s-MacBookAir.local>
2020-11-17 10:34:32 +0900

    Added on association between user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_012938) do
+ActiveRecord::Schema.define(version: 2020_11_17_013207) do
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
